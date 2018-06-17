c9488b24d947f8eaf1ea5306970fc40c6b842ef2
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-17 18:58:52 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,44 @@
 
 # Code your methods below
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+  end
+
+def base_footwork
+  puts "1. Lift right foot
+        2. Return right foot to the ground
+        3. Finishing with a small skip-step backward"
+  end
+
+def skip_step()
+  puts "1. Lower left foot to the ground
+        2. Bounce left foot back up slightly, kicking it a few inches back
+        3. Left, Right, Left and Left"
+  end
+
+def skip_step_left
+  puts "1. Lower right foot to the ground
+        2. Bounce right foot back up slightly, kicking it a few inches back
+        3. Right, Left, Right and Right"
+  end
+
+def bob_the_reins
+    puts "1. Holding your arms out in front of you, straight and     at chest level
+          2. Cross your right wrist over your left and hold them together
+          3. Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+    end
+
+def lasso 
+  puts "1. Start arm movement by holding left arm so the back of       your wrist is near your chin, pointing your left elbow       straight left.
+        2. Lift right arm up and point right elbow diagonally to the right.
+        3. Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+  end
+
+starting_stance
+base_footwork
+bob_the_reins
+skip_step
+skip_step
+lasso
+skip_step_right
+skip_step_left
